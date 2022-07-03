d37476b4c523a327ac50fac6cbaa8ef35164923b
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-03 11:29:09 -0500

    Commented Hello World code

---
+++ Introduction/1.HelloWorld.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //The #include is a preprocessor directive used to include files in our program.
+
+int main() { //A valid C++ program must have the main() function. The execution of code beings from this function.
+    std::cout << "Hello World!"; //std::cout prints the content inside the quotation marks.
+    return 0; //The return 0; statement is the "Exit status" of the program.
+}
