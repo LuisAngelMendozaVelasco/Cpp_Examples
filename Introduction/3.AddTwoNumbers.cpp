6d4c8872e0dc367a5adc7e0b0e71abc269929fa1
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-03 12:38:32 -0500

    Ask for integers separately

--- Introduction/3.AddTwoNumbers.cpp
+++ Introduction/3.AddTwoNumbers.cpp
@@ -8,14 +8,18 @@ using namespace std;
 int main() {
   int first_number, second_number, sum;
     
-  cout << "Enter two integers: ";
-  cin >> first_number >> second_number;
+  //Ask for integers separately
+  cout << "Enter two integers!" << endl;
+  cout << "First integer: ";
+  cin >> first_number;
+  cout << "Second integer: ";
+  cin >> second_number;
 
   // Sum of two numbers
   sum = first_number + second_number;
 
   // Print result
-  cout << first_number << " + " <<  second_number << " = " << sum;     
+  cout << first_number << " + " <<  second_number << " = " << sum << endl;     
 
   return 0;
 }
