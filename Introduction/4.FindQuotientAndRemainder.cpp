28a4b2f989bd19e48bae25cff63320e8a3617534
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-03 21:31:34 -0500

    Code to find quotient and remainder

---
+++ Introduction/4.FindQuotientAndRemainder.cpp
@@ -0,0 +1,29 @@
+//The user is asked to enter two integers (divisor and dividend) and the quotient and the remainder of their division is computed.
+//To compute quotient and remainder, both divisor and dividend should be integers.
+
+#include <iostream>
+using namespace std;
+
+void askOS() {
+    #ifdef _WIN32 //Ask for the OS
+        system("pause");
+    #endif
+}
+
+int main() {    
+    int divisor, dividend, quotient, remainder;
+
+    cout << "Enter dividend: ";
+    cin >> dividend;
+    cout << "Enter divisor: ";
+    cin >> divisor;
+
+    quotient = dividend / divisor; //The division operator / computes the quotient (either between float or integer variables).
+    remainder = dividend % divisor; //The modulus operator % computes the remainder when one integer is divided by another (modulus operator cannot be used for floating-type variables).
+
+    cout << "Quotient = " << quotient << endl;
+    cout << "Remainder = " << remainder << endl;
+
+    askOS();
+    return 0;
+}
