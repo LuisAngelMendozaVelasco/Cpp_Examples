0dc6de4d719cf5a914c1f82c090714879a4af6b5
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-05 11:12:45 -0500

    Code to Multiply two Numbers

---
+++ Introduction/8.MultiplyTwoNumbers.cpp
@@ -0,0 +1,28 @@
+/*In this program, user is asked to enter two numbers (floating point numbers). 
+Then, the product of those two numbers is stored in a variable and displayed on the screen.*/
+
+#include <iostream>
+using namespace std;
+
+void askOS() {
+    #ifdef _WIN32 //Ask for the OS
+        system("pause");
+    #endif
+}
+
+int main() {
+    double num1, num2, product;
+
+    cout << "Enter two numbers!" << endl;
+    //Stores two floating point numbers in num1 and num2 respectively
+    cout << "First number: ";
+    cin >> num1;
+    cout << "Second number: ";
+    cin >> num2; 
+
+    product = num1 * num2;  //Performs multiplication and stores the result in product variable
+    cout << "\nProduct = " << product << endl;    
+
+    askOS();
+    return 0;
+}
