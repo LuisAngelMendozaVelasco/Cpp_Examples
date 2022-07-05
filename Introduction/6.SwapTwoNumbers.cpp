adee871b7260d01a47ceb4164c0e3eaeb3ce141c
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-05 10:34:12 -0500

    Code to Swap Two Numbers

---
+++ Introduction/6.SwapTwoNumbers.cpp
@@ -0,0 +1,30 @@
+/*This example contains a technique to swap numbers in C programming.
+To perform swapping in above example, three variables are used.
+The contents of the first variable is copied into the temp variable. Then, the contents of second variable is copied to the first variable.
+Finally, the contents of the temp variable is copied back to the second variable which completes the swapping process.*/
+
+#include <iostream>
+using namespace std;
+
+void askOS() {
+    #ifdef _WIN32 //Ask for the OS
+        system("pause");
+    #endif
+}
+
+int main() {
+    int a = 5, b = 10, temp;
+
+    cout << "Before swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    askOS();
+    return 0;
+}
