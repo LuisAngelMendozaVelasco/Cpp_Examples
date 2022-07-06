1823f54883bebe56a908f62e2a92d7bed4c18a17
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-05 20:22:55 -0500

    Code to Display Fibonacci Series

---
+++ DecisionsAndLoops/9.DisplayFibonacciSeries.cpp
@@ -0,0 +1,38 @@
+/*The Fibonacci sequence is a series where the next term is the sum of pervious two terms. 
+The first two terms of the Fibonacci sequence is 0 followed by 1.*/
+
+#include <iostream>
+#include "myFunctions.h"
+using namespace std;
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    cout << "\nFibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        // Prints the first two terms.
+        if (i == 1) {
+            cout << t1 << ", ";
+            continue;
+        }
+        if (i == 2) {
+            cout << t2 << ", ";
+            continue;
+        }
+
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+
+        if (i==n)
+            cout << nextTerm << endl;
+        else
+            cout << nextTerm << ", ";
+    }
+
+    askOS();
+    return 0;
+}
