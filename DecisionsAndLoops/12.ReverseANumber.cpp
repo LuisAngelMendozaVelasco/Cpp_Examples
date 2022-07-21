539c4a71761c35cf904f63bff9fc818d6ef6b214
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-20 21:27:02 -0500

    Code to Reverse a Number

---
+++ DecisionsAndLoops/12.ReverseANumber.cpp
@@ -0,0 +1,26 @@
+/*This program takes an integer input from the user and stores it in variable n.
+Then the while loop is iterated until n != 0 is false.
+In each iteration, the remainder when the value of n is divided by 10 is calculated, 
+reversed_number is computed and the value of n is decreased 10 fold.*/
+
+#include <iostream>
+#include "myFunctions.h"
+using namespace std;
+
+int main() {
+    int n, reversed_number = 0, remainder;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    while (n != 0) {
+        remainder = n % 10;
+        reversed_number = reversed_number * 10 + remainder;
+        n /= 10;
+    }
+
+    cout << "\nReversed Number = " << reversed_number <<endl;
+
+    askOS();
+    return 0;
+}
