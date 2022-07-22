ba85a6150cfb8dcbee7c4f04994ab6e7a5a403c9
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-21 20:26:53 -0500

    Code to Display Factors of a Number

---
+++ DecisionsAndLoops/19.DisplayFactorsOfANumber.cpp
@@ -0,0 +1,27 @@
+//This program takes a positive integer from an user and displays all the factors of that number.
+
+#include <iostream>
+#include "myFunctions.h"
+using namespace std;
+
+int main() {
+    int n, i;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    cout << "\nFactors of \"" << n << "\" are: " << endl;  
+    for(i = 1; i <= n; ++i) {
+        if(n % i == 0) {
+            if (i < n) {
+                cout << i << ", ";
+            }
+            else {
+                cout << i << "." << endl;
+            }
+        }     
+    }
+
+    askOS();
+    return 0;
+}
