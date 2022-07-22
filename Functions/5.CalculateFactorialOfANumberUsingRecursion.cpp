171b483aacc47d782bc3faef499f0b1ade04c51a
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-22 16:13:17 -0500

    Code to Calculate Factorial of a Number
    Using Recursion

---
+++ Functions/5.CalculateFactorialOfANumberUsingRecursion.cpp
@@ -0,0 +1,28 @@
+//Example to find factorial of a non-negative integer (entered by the user) using recursion.
+
+#include <iostream>
+#include "myFunctions.h"
+using namespace std;
+
+unsigned long factorial(int n);
+
+int main() {
+    int n;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    cout << "\nFactorial of \"" << n << "\" = " << factorial(n) << endl;
+
+    askOS();
+    return 0;
+}
+
+unsigned long factorial(int n) {
+    if(n > 1) {
+        return n * factorial(n - 1);
+    }
+    else {
+        return 1;
+    }
+}
