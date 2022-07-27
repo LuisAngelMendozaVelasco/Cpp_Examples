8258ae758a37687eb785b36fc015d4a52fa835bd
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-26 19:34:55 -0500

    Code to Find the Length of a String

---
+++ ArraysAndStrings/13.FindTheLengthOfAString.cpp
@@ -0,0 +1,16 @@
+/*You can get the length of a string object by using a size() function or a length() function.
+The size() and length() functions are just synonyms and they both do exactly same thing.*/
+
+#include <iostream>
+#include "myFunctions.h"
+using namespace std;
+
+int main() {
+    string str = "C++ Programming";
+
+    //You can also use str.length()
+    cout << "String length of \"" << str << "\" = " << str.size() << endl;
+
+    askOS();
+    return 0;
+}
