3729244237543801823b2691b10fd9df61312b25
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:42:04 -0500

    Comments - Example 1

---
+++ W3Schools/Comments/Example_1.cpp
@@ -0,0 +1,13 @@
+//Single-line comment before a line of code
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    // This is a comment
+    cout << "Hello World!" << endl;
+
+    askOS();
+    return 0;
+}
