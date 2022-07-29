a280a5ccf0a5d84773a26a9c43585a1aefc32b7e
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-29 15:53:33 -0500

    Strings - Example 4

---
+++ W3Schools/Strings/Example_4.cpp
@@ -0,0 +1,15 @@
+//Access string characters
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    string myString = "Hello";
+
+    cout << myString[0] << endl;
+
+    askOS();
+    return 0;
+}
