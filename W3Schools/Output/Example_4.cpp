4620314978c8393f15e6a4074149a69bb326d3ea
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:39:28 -0500

    Output - Example 4

---
+++ W3Schools/Output/Example_4.cpp
@@ -0,0 +1,13 @@
+//Insert a new line with endl
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "I am learning C++" << endl;
+
+    askOS();
+    return 0;
+}
