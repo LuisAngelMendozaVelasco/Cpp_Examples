24de1b8424e627fda0498ce7af5fd00aff8e0eed
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:48:57 -0500

    Comments - Example 3

---
+++ W3Schools/Comments/Example_3.cpp
@@ -0,0 +1,14 @@
+//Multi-line comment
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    /* The code below will print the words Hello World!
+    to the screen, and it is amazing */  
+    cout << "Hello World!" << endl;
+
+    askOS();
+    return 0;
+}
