7d788a564425d14f71c7825ec82b59a7daf687ae
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:25:05 -0500

    Syntax - Example 1

---
+++ W3Schools/Syntax/Example_1.cpp
@@ -0,0 +1,12 @@
+//Create a simple "Hello World" program
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    askOS();
+    return 0;
+}
