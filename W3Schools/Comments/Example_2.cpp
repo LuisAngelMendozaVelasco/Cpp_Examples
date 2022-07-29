7c0f355a225b0728f8481608697793fbfa809628
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:45:35 -0500

    Comments - Example 2

---
+++ W3Schools/Comments/Example_2.cpp
@@ -0,0 +1,12 @@
+//Single-line comment at the end of a line of code
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl; // This is a comment
+
+    askOS();
+    return 0;
+}
