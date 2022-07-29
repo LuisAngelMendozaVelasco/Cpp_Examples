7a6b2a241d33cf081457f47e39c2235acc2ec4c9
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 20:44:08 -0500

    Variables - Example 4

---
+++ W3Schools/Variables/Example_4.cpp
@@ -0,0 +1,14 @@
+//Create an unchangeable variable with the const keyword
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    const int myNum = 15;
+    
+    cout << myNum << endl;
+
+    askOS();
+    return 0;
+}
