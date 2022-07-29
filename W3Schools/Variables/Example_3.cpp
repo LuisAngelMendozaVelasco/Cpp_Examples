ec5bc38a7aff21551d11906ca07da963bf9775a7
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 20:03:45 -0500

    Variables - Example 3

---
+++ W3Schools/Variables/Example_3.cpp
@@ -0,0 +1,15 @@
+//Assign a new value to an existing value (this will overwrite the previous value)
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int myNum = 15;   // Now myNum is 15
+
+    myNum = 10;       // Now myNum is 10
+    cout << myNum << endl;
+
+    askOS();
+    return 0;
+}
