beff0e5a294fa4ef6d31ac9458152770068a6f83
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 20:58:48 -0500

    Variables - Example 7

---
+++ W3Schools/Variables/Example_7.cpp
@@ -0,0 +1,14 @@
+//Declare many variables of the same type with a comma-separated list
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int x = 5, y = 6, z = 50;
+
+    cout << x + y + z << endl;
+
+    askOS();
+    return 0;
+}
