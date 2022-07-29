6903128899aed57bce43bc683e3acd3a5804434e
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:51:55 -0500

    Variables - Example 1

---
+++ W3Schools/Variables/Example_1.cpp
@@ -0,0 +1,14 @@
+//Create an integer variable
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int myNum = 15;
+    
+    cout << myNum << endl;
+
+    askOS();
+    return 0;
+    }
