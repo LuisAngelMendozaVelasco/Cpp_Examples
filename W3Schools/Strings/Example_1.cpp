ff42ee19db131f2ef66214945a523401ce721722
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-29 15:47:58 -0500

    Strings - Example 1

---
+++ W3Schools/Strings/Example_1.cpp
@@ -0,0 +1,15 @@
+//Create a string
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    string greeting = "Hello";
+
+    cout << greeting << endl;
+
+    askOS();
+    return 0;
+}
