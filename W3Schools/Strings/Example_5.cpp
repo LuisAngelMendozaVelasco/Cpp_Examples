88b33dd3cbef8fe78af1b96750af16842ca3ebca
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-29 16:36:17 -0500

    Strings - Example 5

---
+++ W3Schools/Strings/Example_5.cpp
@@ -0,0 +1,16 @@
+//Change string characters
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+  string myString = "Hello";
+  
+  myString[0] = 'J';
+  cout << myString << endl;
+
+  askOS();
+  return 0;
+}
