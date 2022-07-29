953c87a035a41a63a807e087c47bdcced63b3425
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:35:05 -0500

    Output - Example 2

---
+++ W3Schools/Output/Example_2.cpp
@@ -0,0 +1,13 @@
+//Using many cout objects
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "I am learning C++" << endl;
+
+    askOS();
+    return 0;
+}
