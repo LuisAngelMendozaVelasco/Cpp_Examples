75940c04265a806e37439835592df5c99f8d586c
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-29 15:40:40 -0500

    Operators - Example 3

---
+++ W3Schools/Operators/Example_3.cpp
@@ -0,0 +1,14 @@
+//Assignment operator
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int x = 10;
+
+    cout << x << endl;
+
+    askOS();
+    return 0;
+}
