93507fbcbd1aefff9f05af7be331fa85a3892b27
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 20:50:23 -0500

    Variables - Example 5

---
+++ W3Schools/Variables/Example_5.cpp
@@ -0,0 +1,14 @@
+//Combine text and a variable on print
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int myAge = 35;
+
+    cout << "I am " << myAge << " years old." << endl;
+
+    askOS();
+    return 0;
+}
