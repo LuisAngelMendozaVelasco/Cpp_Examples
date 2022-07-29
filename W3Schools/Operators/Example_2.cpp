49efa984b9eb2d3ea2f902479d8cef2dcb59dba5
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-29 15:39:47 -0500

    Operators - Example 2

---
+++ W3Schools/Operators/Example_2.cpp
@@ -0,0 +1,15 @@
+//Increment operator
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int x = 10;
+
+    ++x;
+    cout << x << endl;
+
+    askOS();
+    return 0;
+}
