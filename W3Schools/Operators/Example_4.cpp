c8b1e5bac4fc17ade928287f56c0af170e8356c3
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-29 15:43:27 -0500

    Operators - Example 4

---
+++ W3Schools/Operators/Example_4.cpp
@@ -0,0 +1,15 @@
+//Addition assignment operator
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int x = 10;
+
+    x += 5;
+    cout << x << endl;
+
+    askOS();
+    return 0;
+}
