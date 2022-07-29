f8f211d6bfbab52673663ade6b539a985b516e07
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:59:51 -0500

    Variables - Example 2

---
+++ W3Schools/Variables/Example_2.cpp
@@ -0,0 +1,15 @@
+//Create a variable without assigning the value, and assign the value later
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int myNum;
+
+    myNum = 15;
+    cout << myNum << endl;
+
+    askOS();
+    return 0;
+}
