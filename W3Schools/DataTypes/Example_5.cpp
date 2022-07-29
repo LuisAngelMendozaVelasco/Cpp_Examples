c07863f3f46b082636b2a6ef9fe7cb732e48f531
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-29 15:12:09 -0500

    DataTypes - Example 5

---
+++ W3Schools/DataTypes/Example_5.cpp
@@ -0,0 +1,17 @@
+//Create boolean types
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout << isCodingFun << "\n";
+    cout << isFishTasty << endl;
+    cout << "Size of bool: " << sizeof(bool) << " bytes" << endl;
+
+    askOS();
+    return 0;
+}
