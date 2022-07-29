00ff99f58b9ef9633dff620e13a1b8328c633789
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 20:57:44 -0500

    Variables - Example 6

---
+++ W3Schools/Variables/Example_6.cpp
@@ -0,0 +1,16 @@
+//Add a variable to another variable
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+
+    cout << sum << endl;
+
+    askOS();
+    return 0;
+}
