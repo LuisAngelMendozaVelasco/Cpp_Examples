df40eda2c7b71ee7aab22f7fb944f1f8481752cd
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-28 19:29:44 -0500

    Output - Example 1

---
+++ W3Schools/Output/Example_1.cpp
@@ -0,0 +1,12 @@
+//Use cout to output values/print text
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    askOS();
+    return 0;
+}
