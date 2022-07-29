db169c849e721c10caf60af8ab72c44bac902134
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-07-29 15:51:19 -0500

    Strings - Example 3

---
+++ W3Schools/Strings/Example_3.cpp
@@ -0,0 +1,15 @@
+//String length
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    cout << "The length of the txt string is: " << txt.length() << endl;
+
+    askOS();
+    return 0;
+}
