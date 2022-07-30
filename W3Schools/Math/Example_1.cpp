4e4a5c7b79f972cd6a967e58e669d8a8aac84b52
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-29 22:55:13 -0500

    Math - Example 1

---
+++ W3Schools/Math/Example_1.cpp
@@ -0,0 +1,12 @@
+//Find the highest value of two numbers
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    cout << max(5, 10) << endl;
+
+    askOS();
+    return 0;
+}
