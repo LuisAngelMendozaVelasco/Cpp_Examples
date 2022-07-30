6ce1d743b8cf8f20c6894c03e2da21919fdebdc2
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-29 23:49:33 -0500

    Conditions - Example 3

---
+++ W3Schools/Conditions/Example_3.cpp
@@ -0,0 +1,22 @@
+//The else if statement
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int time = 22;
+
+    if(time < 10) {
+        cout << "Good morning." << endl;
+    } 
+    else if(time < 20) {
+        cout << "Good day." << endl;
+    } 
+    else {
+        cout << "Good evening." << endl;
+    }
+
+    askOS();
+    return 0;
+}
