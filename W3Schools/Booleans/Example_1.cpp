802e9edb70252d8622d0072a221054d1af850fed
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-29 23:20:19 -0500

    Booleans - Example 1

---
+++ W3Schools/Booleans/Example_1.cpp
@@ -0,0 +1,16 @@
+//Boolean values
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout << isCodingFun << "\n";
+    cout << isFishTasty << endl;
+
+    askOS();
+    return 0;
+}
