e4b7f27e3d139782b8afceb601181c73014fec00
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-29 23:31:19 -0500

    Booleans - Example 3

---
+++ W3Schools/Booleans/Example_3.cpp
@@ -0,0 +1,15 @@
+//Compare two variables
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 9;
+
+    cout << (x > y) << endl;
+
+    askOS();
+    return 0;
+}
