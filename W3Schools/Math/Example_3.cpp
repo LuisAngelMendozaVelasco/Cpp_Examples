81a62266dc4902b0ac8766a191e0255125e7bc6b
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-29 23:02:47 -0500

    Math - Example 3

---
+++ W3Schools/Math/Example_3.cpp
@@ -0,0 +1,15 @@
+//Use the cmath header file for other math functions
+
+#include <iostream>
+#include <cmath>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    cout << sqrt(64) << "\n";
+    cout << round(2.6) << "\n";
+    cout << log(2) << "\n";
+
+    askOS();
+    return 0;
+}
