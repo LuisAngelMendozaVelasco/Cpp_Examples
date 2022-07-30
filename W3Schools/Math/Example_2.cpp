3ce5b242bc4ceb697883e2882d813524b098784e
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-29 22:59:12 -0500

    Math - Example 2

---
+++ W3Schools/Math/Example_2.cpp
@@ -0,0 +1,12 @@
+//Find the lowest value of two numbers
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    cout << min(5, 10) << endl;
+
+    askOS();
+    return 0;
+}
