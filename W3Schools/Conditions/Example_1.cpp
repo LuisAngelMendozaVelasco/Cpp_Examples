2423dfa26191b750181c9598a7b7b523c91c9dbc
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-29 23:39:27 -0500

    Conditions - Example 1

---
+++ W3Schools/Conditions/Example_1.cpp
@@ -0,0 +1,14 @@
+//The if statement
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    if(20 > 18) {
+        cout << "20 is greater than 18" << endl;
+    }  
+
+    askOS();
+    return 0;
+}
