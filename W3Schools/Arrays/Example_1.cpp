52d52c85982345a7a32290d09b8c1ac3b4de84cb
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-30 23:48:15 -0500

    Arrays - Example 1

---
+++ W3Schools/Arrays/Example_1.cpp
@@ -0,0 +1,15 @@
+//Create and access an array
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+
+    cout << cars[0] << endl;
+
+    askOS();
+    return 0;
+}
