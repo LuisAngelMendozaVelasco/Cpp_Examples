e11244a2058193ccdae649b2edd73f903255c334
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-31 12:42:48 -0500

    References - Example 1

---
+++ W3Schools/References/Example_1.cpp
@@ -0,0 +1,21 @@
+//Create a reference variable
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    /*A reference variable is a "reference" to an existing variable, 
+    and it is created with the & operator*/
+    string food = "Pizza";
+    string &meal = food;
+
+    /*Now, we can use either the variable name food or the reference 
+    name meal to refer to the food variable*/
+    cout << food << "\n";
+    cout << meal << "\n";
+
+    askOS();
+    return 0;
+}
