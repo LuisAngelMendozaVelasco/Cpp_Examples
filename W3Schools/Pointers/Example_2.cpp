b0cd9d7c4c6ea16cfd2e2dae6a5e3bea1b6561c6
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-31 13:05:55 -0500

    Pointers - Example 2

---
+++ W3Schools/Pointers/Example_2.cpp
@@ -0,0 +1,20 @@
+//Get the value of a variable with the dereference operator
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    string food = "Pizza";  // Variable declaration
+    string* ptr = &food;    // Pointer declaration
+
+    // Reference: Output the memory address of food with the pointer
+    cout << ptr << "\n";
+
+    // Dereference: Output the value of food with the pointer
+    cout << *ptr << "\n";
+
+    askOS();
+    return 0;
+}
