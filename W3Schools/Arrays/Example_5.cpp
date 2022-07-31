cc965ab62f98abe6dacade41bdf34f3dbbb4989e
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-31 00:11:39 -0500

    Arrays - Example 5

---
+++ W3Schools/Arrays/Example_5.cpp
@@ -0,0 +1,15 @@
+//Multidimensional arrays
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    string letters[2][4] = {{ "A", "B", "C", "D" },
+                            { "E", "F", "G", "H" }};
+
+    cout << letters[0][2] << endl;
+
+    askOS();
+    return 0;
+}
