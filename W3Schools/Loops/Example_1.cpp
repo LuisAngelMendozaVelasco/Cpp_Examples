611e9a49b540a1656542a5010fa335e62c8e9eb7
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-30 22:44:12 -0500

    Loops - Example 1

---
+++ W3Schools/Loops/Example_1.cpp
@@ -0,0 +1,17 @@
+//While loop
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    int i = 0;
+
+    while(i < 5) {
+    cout << i << "\n";
+    i++;
+    }
+
+    askOS();
+    return 0;
+}
