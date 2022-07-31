05d15f8101fdb7208632ab5521defd569a493038
Luis Mendoza <luisangel.mendozavelasco@outlook.com>
2022-07-30 23:22:39 -0500

    Loops - Example 3

---
+++ W3Schools/Loops/Example_3.cpp
@@ -0,0 +1,14 @@
+//For loop
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+int main() {
+    for(int i = 0; i < 5; i++) {
+    cout << i << "\n";
+    }
+
+    askOS();
+    return 0;
+}
