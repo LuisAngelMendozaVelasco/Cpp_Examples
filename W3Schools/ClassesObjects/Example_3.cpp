2d336c0a0aa43529ba93feb7c574badce2eb5fc0
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-02 18:38:25 +0200

    ClassesObjects - Example 3

---
+++ W3Schools/ClassesObjects/Example_3.cpp
@@ -0,0 +1,21 @@
+//Create class methods
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+class MyClass {         // The class
+    public:               // Access specifier
+        void myMethod() {   // Method/function
+            cout << "Hello World!" << endl;
+        }
+};
+
+int main() {
+    MyClass myObj;     // Create an object of MyClass
+    
+    myObj.myMethod();  // Call the method
+
+    askOS();
+    return 0;
+}
