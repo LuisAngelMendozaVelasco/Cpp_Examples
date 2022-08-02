da340a8e73923aa79d9ff840906b3893f1b27e14
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-02 18:56:51 +0200

    ClassesObjects - Example 4

---
+++ W3Schools/ClassesObjects/Example_4.cpp
@@ -0,0 +1,24 @@
+//Define a class method outside the class definition
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+class MyClass {         // The class
+    public:               // Access specifier
+        void myMethod();    // Method/function declaration
+};
+
+// Method/function definition outside the class
+void MyClass::myMethod() {
+    cout << "Hello World!" << endl;
+}
+
+int main() {
+    MyClass myObj;     // Create an object of MyClass
+
+    myObj.myMethod();  // Call the method
+
+    askOS();
+    return 0;
+}
