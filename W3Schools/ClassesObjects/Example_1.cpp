84f720e16de011af3545ccd639efc8604ab73950
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-02 18:31:48 +0200

    ClassesObjects - Example 1

---
+++ W3Schools/ClassesObjects/Example_1.cpp
@@ -0,0 +1,27 @@
+//Create an object of a class and access class attributes<
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h" 
+using namespace std;
+
+class MyClass {       // The class
+    public:             // Access specifier
+        int myNum;        // Attribute (int variable)
+        string myString;  // Attribute (string variable)
+};
+
+int main() {
+    MyClass myObj;  // Create an object of MyClass
+
+    // Access attributes and set values
+    myObj.myNum = 15;
+    myObj.myString = "Some text";
+
+    // Print values
+    cout << myObj.myNum << "\n"; 
+    cout << myObj.myString << endl; 
+
+    askOS();
+    return 0;
+}
