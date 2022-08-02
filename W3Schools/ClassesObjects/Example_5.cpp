7562c23a145bc91a127f5f5944d00ec1561946e1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-02 18:59:31 +0200

    ClassesObjects - Example 5

---
+++ W3Schools/ClassesObjects/Example_5.cpp
@@ -0,0 +1,23 @@
+//Add parameters to a class method
+
+#include <iostream>
+#include "../myFunctions.h"
+using namespace std;
+
+class Car {
+    public:
+        int speed(int maxSpeed);
+};
+
+int Car::speed(int maxSpeed) {
+    return maxSpeed;
+}
+
+int main() {
+    Car myObj;
+
+    cout << myObj.speed(200) << endl;
+
+    askOS();
+    return 0;
+}
