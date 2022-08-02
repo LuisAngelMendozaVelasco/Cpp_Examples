d9f4b665da3ccdce31913c937d6acac11a7f5f89
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-02 17:48:01 +0200

    Functions - Example 10

---
+++ W3Schools/Functions/Example_10.cpp
@@ -0,0 +1,20 @@
+//Pass an array to a function
+
+#include <iostream> 
+#include "../myFunctions.h" 
+using namespace std;  
+
+void myFunction(int myNumbers[5]) {
+    for(int i = 0; i < 5; i++) {  
+        cout << myNumbers[i] << "\n";    
+    } 
+}
+
+int main() {  
+    int myNumbers[5] = {10, 20, 30, 40, 50};  
+    
+    myFunction(myNumbers);
+
+    askOS();
+    return 0;
+}  
