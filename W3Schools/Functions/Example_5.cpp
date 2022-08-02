87336e4f17cc329230a602b8adf8531ff85f2c31
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-02 17:08:46 +0200

    Functions - Example 5

---
+++ W3Schools/Functions/Example_5.cpp
@@ -0,0 +1,20 @@
+//Default parameter value
+
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+using namespace std;
+
+void myFunction(string country = "Norway") {
+    cout << country << "\n";
+}
+
+int main() {
+    myFunction("Sweden");
+    myFunction("India");
+    myFunction();
+    myFunction("USA");
+
+    askOS();
+    return 0;
+}
