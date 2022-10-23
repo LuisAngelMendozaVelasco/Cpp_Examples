6881cf829ec0d9687e9e7071f4378041bded2733
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2022-10-23 10:43:51 -0500

    Delete Programiz directory

--- Programiz/Introduction/1.HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> //The #include is a preprocessor directive used to include files in our program.
-#include "../myFunctions.h"
-
-int main() { //A valid C++ program must have the main() function. The execution of code beings from this function.
-    std::cout << "Hello World!" << std::endl; //std::cout prints the content inside the quotation marks.
-
-    askOS();
-    return 0; //The return 0; statement is the "Exit status" of the program.
-}
